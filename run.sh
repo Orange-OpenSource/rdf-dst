a5bb96992f40eff13a75e9113e7e0b49a3389b5c
Andres Gonzalez <andres.gonzalez@orange.com>
2023-07-30 21:25:39 +0200

    improved shell scripting

--- run.sh
+++ run.sh
@@ -4,25 +4,30 @@
 DIR=./dst-snake
 
 # default values, workers must be 1 with marcel... 6 with nadia?
-experiment="${experiment:-1}"
+#experiment="${experiment:-1}"
 workers=5
-framework="baseline"
-model="t5"  # t5, flant-t5, long-t5-local, long-t5-tglobal
+#framework="baseline"
+#model="t5"  # t5, flant-t5, long-t5-local, long-t5-tglobal
 
 programname=$0
 function usage {
     echo ""
     echo "Runs DST experiments to generate DST as RDFs"
     echo ""
-    echo "usage: $programname --debug string --experiment integer --devices integer"
+    echo "usage: $programname --debug string --experiment integer --setup string --framework string --model string --size string"
     echo ""
-    echo "  --setup          	        training or evaluating"
+    echo "  --setup string    	        training or evaluating"
     echo "                              (example: train, evaluate)"
-    echo "  --framework string          using HF, torch, or lightning"
+    echo "  --framework string          using HF, torch, baseline or lightning"
     echo "                              (example: pl, torch, hf)"
-    echo "  --debug string   		 yes or no"
+    echo "  --debug string   		    yes or no"
     echo "                              (example: no)"
-    echo "  --experiment integer        which experiment to run. 1, 2, or 3" echo "                              (example and default val: 1)"
+    echo "  --model string              t5, flan-t5, long-t5-local or long-t5-tglobal"
+    echo "                              (example: long-t5-tglobal)"
+    echo "  --size string               small, base, large. NO small version available for long models"
+    echo "                              (example: small)"
+    echo "  --experiment integer        which experiment to run. 1, 2, or 3" echo "
+                                       (example and default val: 1)"
     echo ""
 }
 function die {
@@ -43,9 +48,9 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-if [[ -z $debug || -z $setup ]]; then
+if [[ -z $debug || -z $setup || -z $model || -z $size ]]; then
     usage
-    die "Missing parameter --debug or --setup"
+    die "Missing parameter --debug or --setup or --model"
 fi
 
 if [[ -z $framework ]]; then
@@ -86,9 +91,9 @@ else
 fi
 
 if [[ $debug == "yes" ]]; then
-    python "$script" -epochs 2 -d multiwoz --batch 16 -store yes -logger no -experiment "$experiment" -workers "$workers" -model "$model" -model_size small -subset yes -device cuda -method online -peft lora
+    python "$script" -epochs 2 -d multiwoz --batch 16 -store yes -logger no -experiment "$experiment" -workers "$workers" -model "$model" -model_size "$size" -subset yes -device cuda -method online -peft lora
 elif [[ $debug == "no" ]]; then
-    python "$script" -epochs 5 --batch 16 -d multiwoz -workers "$workers" -store yes -experiment "$experiment" -model "$model" -model_size base -logger no -subset no -method offline -peft no
+    python "$script" -epochs 5 --batch 16 -d multiwoz -workers "$workers" -store yes -experiment "$experiment" -model "$model" -model_size "$size" -logger no -subset no -method offline -peft lora
 else
     usage
     die "Invalid value for debug parameter"
